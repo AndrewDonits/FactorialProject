ca4a51eb22df9f915ff0bc713977aa8dbf81217b
AndrewDonits <andonits5@gmail.com>
2021-10-04 21:46:44 +0300

    Factorial Project. Returns the factorial of a number in a recursive method

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int num1 = 23;
+        System.out.println("Factorial of " + num1 + " is: " + func(num1));
+
+        int num2 = 5;
+        System.out.println("Factorial of " + num2 + " is: " + func(num2));
+
+        int num3 = 1;
+        System.out.println("Factorial of " + num3 + " is: " + func(num3));
+
+    }
+
+    public static long func (long n) {
+        if (n <= 1)
+            return n;
+        else
+            return n * func(n -1);
+    }
+}
